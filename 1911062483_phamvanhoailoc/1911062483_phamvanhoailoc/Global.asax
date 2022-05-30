811abf9bb474264bea68aef8ab1eaad9fdc06b73
Admin <phamvanhoailoc0507@gmail.com>
2022-05-30 10:39:09 +0700

    upload project firt time

---
+++ 1911062483_phamvanhoailoc/1911062483_phamvanhoailoc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_1911062483_phamvanhoailoc.MvcApplication" Language="C#" %>
---
+++ 1911062483_phamvanhoailoc/1911062483_phamvanhoailoc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_1911062483_phamvanhoailoc.Startup))]
+namespace _1911062483_phamvanhoailoc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
